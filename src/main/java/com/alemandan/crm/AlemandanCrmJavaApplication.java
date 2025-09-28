baf1eaeaffde170b27c8636982a9d833850637ff
Daniel <danielgarciasu1029@gmail.com>
2025-09-28 12:25:28 -0500

    Proyecto base creado con dependencias principales

---
+++ src/main/java/com/alemandan/crm/AlemandanCrmJavaApplication.java
@@ -0,0 +1,13 @@
+package com.alemandan.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlemandanCrmJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlemandanCrmJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alemandan/crm/AlemandanCrmJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alemandan.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlemandanCrmJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
